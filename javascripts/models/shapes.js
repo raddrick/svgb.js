38687fb88a90ff54b5cb45de4dc0e98e67a72787
Rick Graham <rick@raddstudio.com>
2012-07-18 22:10:10 -0400

    shape objects inheriting from point
    initial commit of path, line, polyline, polygon, circle, rect, and
    elipse

---
+++ javascripts/models/shapes.js
@@ -0,0 +1,85 @@
+(function($){
+
+// path
+  window.svgb.Path = window.svgb.Point.extend({
+    defaults: {
+      'd': ""
+    },    
+    initialize: function(){
+      
+    }
+  });
+
+// line
+//   x2
+//   y2
+  window.svgb.Line = window.svgb.Point.extend({
+    defaults: {
+      'x2': 0,
+      'y2': 0
+    },    
+    initialize: function(){
+      
+    }
+  });
+
+// polyline
+//   points
+  window.svgb.Ployline = window.svgb.Point.extend({
+    defaults: {
+      'points':[]
+    },    
+    initialize: function(){
+      
+    }
+  });
+
+// polygon
+//   points
+  window.svgb.Ploygon = window.svgb.Point.extend({
+    defaults: {
+      'points': []
+    },    
+    initialize: function(){
+      
+    }
+  });
+
+// circle
+//   r
+  window.svgb.Circle = window.svgb.Point.extend({
+    defaults: {
+      'r': 0
+    },    
+    initialize: function(){
+      
+    }
+  });
+    
+// rect
+//   height
+//   width
+  window.svgb.Rect = window.svgb.Point.extend({
+    defaults: {
+      'width':500,
+      'height':500
+    },    
+    initialize: function(){
+      
+    }
+  });
+
+// elipse
+//   rx
+//   ry
+  window.svgb.Elipse = window.svgb.Point.extend({
+    defaults: {
+      'rx':0,
+      'ry':0
+    },    
+    initialize: function(){
+      
+    }
+  });
+
+})(jQuery);
