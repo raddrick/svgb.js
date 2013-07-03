242b28da13cc2651e2469bd0828b697fbd3cae9c
Rick Graham <rick@raddstudio.com>
2013-07-02 23:41:15 -0400

    remove the jasmine test suite files

--- spec/javascripts/StageSpec.js
+++
@@ -1,7 +0,0 @@
-  // window.svgb.Models.Frame = Backbone.Model.extend({
-  //   defaults: {
-  //     'viewBox': [0,0,200,200],
-  //     'width':500,
-  //     'height':500,
-  //     'masked':true
-  //   },    
