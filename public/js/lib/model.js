3aee54b6c9c5745c01ccb6556ebe2b6d8f860817
Rick Graham <rick@raddstudio.com>
2013-08-03 14:31:09 -0400

    add to_svg to model base class

--- public/js/lib/model.js
+++ public/js/lib/model.js
@@ -1,8 +1,57 @@
 (function() {
   svgb.Model = Class.extend({
     attributes: [], //key,value pairs
-    init: function(){
+    nodes: [],
+    init: function(options){
+      if (options){
+        if (options.nodes) this.nodes = options.nodes;
+      }
+    },
+    append: function(node){
+      console.log(node);
+      if (node) this.nodes.push(node);
+    },
+    to_svg: function(){
+      var str = "";
+      this.el = document.createElementNS(SVGNS, this.attributes.tag);
 
+      function process(key,value,self) {
+        function namespace(key){
+          var ns = key.split(":")[0].toUpperCase() + "NS";
+          
+          switch(ns){
+            case "SVGNS":
+              ns = SVGNS;
+              break;
+            case "XLINKNS":
+              ns = XLINKNS;
+              break;
+            default:
+              ns = SVGNS;
+          }
+          return ns;
+        }
+        // log(key + " : "+value);
+        if (value && key!='tag'){
+          if (key.indexOf(":")>0){
+            self.el.setAttributeNS(namespace(key),key,value);
+          }else{
+            self.el.setAttribute(key,value);
+          }
+        }
+      }
+      function traverse(o,func,self) {
+            
+        for (var i in o) {
+          func.apply(this,[i,o[i],self]);
+          if (typeof(o[i])=="object") {
+            traverse(o[i],func,self);
+          }
+        }
+      }
+
+      traverse(this.attributes,process,this);
+      return this.el;
     }
   });
 })();
