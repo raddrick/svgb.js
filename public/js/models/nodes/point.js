6a3a58545dd9aaf3525f523821338a3f510bb1a9
Rick Graham <rick@raddstudio.com>
2013-08-11 15:30:03 -0400

    turn a point from a circle to a g tag and support setting the transform as the tag's x/y

--- public/js/models/nodes/point.js
+++ public/js/models/nodes/point.js
@@ -7,41 +7,57 @@
     _type:"svgb.model.point",
     events:{},
     defaults: {
-      tag: 'circle',
+      tag: 'g',
       id: '',
       x: 0,
       y: 0,
-      cx: 0,
-      cy: 0,
-      r: 3,
       css: '',
       style: '',
-      defs: '',
-      transform:'' //TODO: support array values for int's
+      defs: ''
     },
     transform: null,
     init: function(options){
+      console.log("MODEL INIT type: " + this._type);
+      this.pin.el=null;
+      this.pin.visible=false;
       this.transform = new svgb.models.Transform({});
-      //if (this.attributes.tag) this.render();
-      if (options.x)      this.defaults.x = options.x;
-      if (options.y)      this.defaults.y = options.y;
-      if (options.css)    this.defaults.css = options.css;
-      if (options.style)  this.defaults.style = options.style;
-      if (options.defs)   this.defaults.defs = options.defs;
-      if (options.transform) this.defaults.transform = options.transform;
-
+      
+      if (options){
+        if (options.x)      this.defaults.x = options.x;
+        if (options.y)      this.defaults.y = options.y;
+        if (options.css)    this.defaults.css = options.css;
+        if (options.style)  this.defaults.style = options.style;
+        if (options.defs)   this.defaults.defs = options.defs;
+        if (options.transform) this.defaults.transform = options.transform;
+        if (options.pin)    this.pin.init();
+      }
       this._super(this);
-      //this.pin.set_attrs();
     },
-    pin:{
-      set_attrs: function(){
-        this.attributes.tag = "circle";
-        this.attributes.css="pin";
-        this.attributes.r=3;
-        this.attributes.cx=this.attributes.x;
-        this.attributes.cy=this.attributes.y;
-        this.attributes.x="";
-        this.attributes.y="";
+    pin: {
+      el: null,
+      visible: false,
+      init: function(pt){
+        this.show();
+        this.create();
+        return 
+
+      },
+      create: function(){
+        // this.el = new svgb.models.Circle({r:3,cx:0,cy:0});
+        this.el = document.createElementNS(SVGNS, 'circle');
+        this.el.setAttribute('r',3);
+        this.el.setAttribute('cx',0);
+        this.el.setAttribute('cy',0);
+
+        return this.el;
+      },
+      show: function(){
+        if (!this.el) this.create();
+        this.visible=true;
+      },
+      hide: function(){
+        if (!this.el) this.create();
+        this.visible=false;
       }
     },
     move: function( add_x, add_y, time_ms ){
@@ -68,6 +84,19 @@
       if (pos.translate)
 
       setTimeout(animate, time_ms );
+    },
+    to_svg: function(create_pin){
+      if (this._type == 'svgb.model.point'){
+        if (create_pin){
+          this.attributes.transform = this.pin.init(this);
+          if (this.pin.el) this.nodes.push(this.pin.el);
+        }
+        this.transform.attr({translate:{x:this.attributes.x, y:this.attributes.y}});
+        this.attributes.transform = this.transform.to_s();
+        delete this.attributes['x'];
+        delete this.attributes['y'];
+      }
+      return this._super();
     }
   });
 })();
